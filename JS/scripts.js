a31969541e87f6e91e92b607204acefd147ccaa8
Josh <jzeilmann93@gmail.com>
2018-01-14 14:34:51 -0600

    Portfolio Site Assets

---
+++ JS/scripts.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+//var firstName = prompt("What is your first name?");
+//var lastName = prompt("What is your last name?");
+//var age = prompt("What is your age?");
+
+//console.log("Your name is " + firstName + " " + lastName);
+//console.log("You are " + age + " years old.");
+
+
+
+//var age = prompt("What is your age?");
+//var days = age * 365;
+//alert(age + " is roughly " + days + " days old");
+
+
+/*var secretNum = 4;
+
+var guess = prompt("Guess a number");
+
+if (guess < secretNum) {
+	alert("You guess was too low");
+}
+
+else if (Number(guess) === secretNum) {
+	alert("You guessed the right number!");
+}
+
+else {
+	alert("Your guess was too high!");
+} 
+
+var count = 0;
+
+while(cou)*/
